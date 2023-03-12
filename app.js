e8f62836eadc3777ddd41f791f386c6f4b7f82a3
CarlHejlesen <113053807+CarlHejlesen@users.noreply.github.com>
2023-03-12 17:20:03 +0100

    Min requriements is done

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 
 import path from "path";
 
-import *as ass from './loginfeature.js';
+//import *as ass from './loginfeature.js';
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 app.set("port", process.env.PORT || 3000);
@@ -11,13 +12,8 @@ app.set("port", process.env.PORT || 3000);
 app.get("/",(req,res)=>{
     res.sendFile(path.resolve() + "/public/html/index.html");
 })
-app.get("/login",(req,res)=>{
-    res.sendFile(path.resolve() + "/public/html/login.html");
-})
-app.post("/login",(req,res)=>{
-    console.log(req);
-    res.redirect("/");
-})
+
+
 
 //New Page for forgot password
 app.get("/forgot",(req,res)=>{
@@ -28,13 +24,52 @@ app.post("/forgot",(req,res)=>{
     res.redirect("/");
 })
 
-//New page for sign up
-app.get("/login",(req,res)=>{
+
+
+
+
+
+
+
+
+
+
+
+//This is Carl && Shadi´s Dont touch
+app.get("/login",(req,res)=>{                                   // this function defines what will be send when "/login" url is accesed  
     res.sendFile(path.resolve() + "/public/html/login.html");
 })
-app.post("/login",(req,res)=>{
-    console.log(req);
-    res.redirect("/");
+
+
+
+import users from './loginfeature.js';// Here we import our read && csv function
+const users44=[];
+users(users44);
+
+//console.log(users44)
+
+
+//
+app.post("/login",(req,res)=>{                                  // This is the post function, this will be activated when ever things that has been include in the form action in the htlm file.
+                                                                 // if then its activated this function will launch 
+let usernametest =req.body.username;
+let passwordtest=req.body.password;
+console.log(usernametest)
+console.log(passwordtest)
+
+let user = users44.find(function(user) {
+    return user.username === usernametest && user.password === passwordtest;
+  });
+
+ //   console.log(req);
+    if(user){
+        res.redirect("/");
+    }
+    else{
+       res.redirect("/login");  
+    }
+       
+    
 })
 
 
--- loginfeature.js
+++ loginfeature.js
@@ -1,24 +1,26 @@
-
+import express from 'express';
+const app = express();
 import fs from 'fs';
 import csv from 'csv-parser';
 
 
+
 // First function will load all username and password into an array
 
-const users=[];
 
-const loginfunction=function(){
+
+const loginfunction=function(users44){
 fs.createReadStream('./Passwords/users.txt')
   .pipe(csv())
   .on('data', (data) => {
-    users.push(data);
+    users44.push(data);
   })
   .on('end', () => {
-    console.log(users);
+    console.log(users44);
   });
 
 }
-loginfunction();
+
 export default loginfunction;
 
 
