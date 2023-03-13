5b7a65f0191f736a1da39414b70fca849fd13320
AlliasGhorbani <113035164+AlliasGhorbani@users.noreply.github.com>
2023-03-13 20:27:19 +0100

    Commit (WIP)
    Til shadi
    (viderarbejde)

---
+++ recipereader.js
@@ -0,0 +1,16 @@
+//import fs from 'fs';
+
+console.log("This obivuos dont work out, so im breaking up with you")
+
+/*
+
+// Read the contents of the file into a variable
+const fileContents = fs.readFileSync('./Recipes/recipes.json', 'utf8');
+
+console.log(fileContents)
+
+// Access the properties of the recipe object
+console.log(recipe.navn); // Outputs "Chicken Alfredo Pasta"
+console.log(recipe.ingredienser[0].ingrediens); // Outputs "chicken breast"
+console.log(recipe.instruktioner[0]); // Outputs "Season chicken with salt and pepper."
+*/
