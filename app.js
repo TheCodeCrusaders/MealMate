71aa2c7b6f412a179e44268096a367df5e5111f2
JeppesGitHub <jreink22@student.aau.dk>
2023-03-15 10:23:30 +0100

    Jeppes local
    Jeppes local

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import express from 'express';
 const app = express();
 import path from "path";
@@ -16,6 +17,7 @@ app.get("/dashboard",(req,res)=>{
 app.get("/login",(req,res)=>{
     res.sendFile(path.resolve() + "/public/html/login.html");
 })
+
 app.post("/login",(req,res)=>{
     console.log(req);
     res.redirect("/");
@@ -39,6 +41,19 @@ app.post("/login",(req,res)=>{
     res.redirect("/");
 })
 
+app.get("/getList", (req, res) => {
+    const filePath = path.resolve() + "/Files to read/Items.json";
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Internal Server Error");
+        } else {
+            const jsonData = data.toString("utf8");
+            res.json(JSON.parse(jsonData));
+        }
+    });
+});
 
 app.listen(app.get('port'), function () {
     console.log('app listening at: ' + "http://localhost:" + app.get('port') + "/");
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,10 +1,58 @@
-const Papa = require('papaparse'); //Imports papaparse library, which can be accesed by "Papa"
+document.addEventListener("DOMContentLoaded", (e) => {
+  fetch("/getList")
+      .then(response => {
+          if (response.ok) {
+              return response.json();
+          }
+          throw new Error("response was not in the 200 range " + response.Error)
+      })
+      .then(data => {
+        // Create table element
+        const table = document.createElement("table");    
+        
+        // Create header row
+        const headerRow = document.createElement("tr");     
+        const header1 = document.createElement("th");
+        header1.textContent = "Location";
+        headerRow.appendChild(header1);
 
-fetch("../CSV/Items.csv")
-.then(response => response.text())
-.then(data => {
-  // Parse the CSV data using PapaParse
-  const parsedData = Papa.parse(data, { header: true });
-  console.log(parsedData);
-});
+        const header2 = document.createElement("th");
+        header2.textContent = "Name";
+        headerRow.appendChild(header2);
+
+        const header3 = document.createElement("th");
+        header3.textContent = "Expiration date";
+        headerRow.appendChild(header3);
+
+        // Add header row to table
+        table.appendChild(headerRow);
+        
+        // Loop through data and create rows
+        data.forEach(item => {
+            const row = document.createElement("tr");
+            const cell1 = document.createElement("td");
+            cell1.textContent = item.location;
+            row.appendChild(cell1);
+            
+            const cell2 = document.createElement("td");
+            cell2.textContent = item.name;
+            row.appendChild(cell2);
+
+            const cell3 = document.createElement("td");
+            cell3.textContent = item.expirationDate;
+            row.appendChild(cell3);
+            
+            // Add row to table
+            table.appendChild(row);
+        });
+        
+        // Add table to HTML page
+        const tableContainer = document.getElementById("Json-table");
+        tableContainer.appendChild(table);
+    })
+    .catch(error => console.error(error));
+})
+
+
+
 
