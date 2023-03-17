84500b1b3e73344f6921a0f1e5129985c4607e95
mathiaszip <73193562+mathiaszip@users.noreply.github.com>
2023-03-17 11:53:03 +0100

    better animation for process bar

--- public/dashboard/dashboard.js
+++ public/dashboard/dashboard.js
@@ -58,12 +58,16 @@ function move(amount) {
     var elem = document.getElementById("myBar");
     var width = 0;
     var id = setInterval(frame, 10);
+
+    
     function frame() {
-        if (width >= amount) {
-            clearInterval(id);
-        } else {
-            width++;
-            elem.style.width = width + '%';
-        }
+        setTimeout(function() {
+            if (width >= amount) {
+                clearInterval(id);
+            } else {
+                width++;
+                elem.style.width = width + '%';
+            }
+        },1000);
     }
 }
