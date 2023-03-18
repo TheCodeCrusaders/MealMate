b9c2342ec6c0ca04637b685a1fdf6d6718a3cafa
mathiaszip <73193562+mathiaszip@users.noreply.github.com>
2023-03-18 11:42:00 +0100

    added registration
    i was bored, so np if not needed

--- routes.js
+++ routes.js
@@ -7,10 +7,6 @@ import cookieParser from 'cookie-parser';
 router.use(cookieParser());
 
 
-
-
-
-
 import recipies from './recipe.js'
 router.post('/API/search', (req, res) => {
     const name = req.body.navn;
@@ -76,8 +72,8 @@ function verifyToken(req, res, next) {
   }
 
 router.post("/login",(req,res)=>{  // post action declared, will wait for post from front end                                
-    let usernametest =req.body.username; //gets username from front end
-    let passwordtest=req.body.password; //gets password from front end
+    let usernametest = req.body.username; //gets username from front end
+    let passwordtest= req.body.password; //gets password from front end
 
     console.log(`${usernametest} tried to login`)// THis will log who tried to loged in or logged in
 
@@ -101,36 +97,26 @@ router.post("/login",(req,res)=>{  // post action declared, will wait for post f
     }
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// getting a list route (still neds to be modified for real login system)
-router.get("/API/getList", (req, res) => {
-    console.log(req.query.name);
-    const filePath = path.resolve() + `/USERS/${req.query.name}/items.json`;
-
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send("Internal Server Error");
-        } else {
-            const jsonData = data.toString("utf8");
-            res.json(JSON.parse(jsonData));
-        }
+router.get("/API/getUserName",verifyToken, (req, res) => {
+    res.json({"username": req.user.username})
+    
+    })
+ 
+    // getting a list route (still neds to be modified for real login system)
+    router.get("/API/getList",verifyToken, (req, res) => {
+        console.log(req.query.name);
+        const filePath = path.resolve() + `/data/USERS/${req.user.username}/items.json`;
+    
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send("Internal Server Error");
+            } else {
+                const jsonData = data.toString("utf8");
+                res.json(JSON.parse(jsonData));
+            }
+        });
     });
-});
 
 
 // write list to file (still neds to be modified for real login system)
@@ -152,5 +138,58 @@ router.post("/postlist", (req, res) => {
 
     res.redirect("/itemtracking");
 });
+
+router.post('/newuser', (req, res) => {
+    // Extract the new user data from the request body
+    const newUser = {
+      username: req.body.username,
+      password: req.body.password
+    };
+  
+    const dataPath = path.join(path.resolve() + "/data/Passwords/users.json");
+    let data = {};
+    try {
+        data = JSON.parse(fs.readFileSync(dataPath));
+    } catch (error) { }
+  
+    // Check for duplicate usernames
+    const duplicate = data.users.find(user => user.username === newUser.username);
+    if (duplicate) {
+      res.body.message.textContent = "ok";
+      return res.status(400).json({ error: 'Username already exists' });
+    }
+  
+    // Add the new data to the array
+    data.users.push(newUser);
+  
+    // Write the updated data back to the JSON file
+    fs.writeFileSync(dataPath, JSON.stringify(data, null, 2));
+
+    fs.mkdir(`data/USERS/${newUser.username}`, (err) => {
+      if (err) throw err;
+    });
+
+    console.log(`${newUser.username} directory created.`);
+
+    fs.writeFile(`data/USERS/${newUser.username}/consumedItems.json`, '', (err) => {
+      if (err) throw err;
+      console.log(`consumedItems.json created in ${newUser.username}`);
+    });
+
+    fs.writeFile(`data/USERS/${newUser.username}/items.json`, '', (err) => {
+      if (err) throw err;
+      console.log(`items.json created in ${newUser.username}`);
+    });
+
+    fs.writeFile(`data/USERS/${newUser.username}/wastedItems.json`, '', (err) => {
+      if (err) throw err;
+      console.log(`wastedItems.json created in ${newUser.username}`);
+    });
+  
+    // Return a response to the client
+    res.status(201).json(newUser);
+  });
+  
+  
 
 export default router
