ec19c8166c18f9728027bc09757c9478425180d9
JeppesGitHub <jreink22@student.aau.dk>
2023-03-27 11:48:42 +0200

    Seperate folders

--- public/barcode-Jeppe/barcode-Jeppe.js
+++ public/barcode-Jeppe/barcode-Jeppe.js
@@ -1,37 +1,15 @@
 //CONSTANT DECLERATIONS
+const postpicplace=document.getElementById("postpicplace");
 const fileInput = document.getElementById('file-input');
 const resultDiv = document.getElementById('result');
 const subbutton=document.getElementById('submitbu');
 const showpic=document.getElementById("showpic");
-const postpicplace=document.getElementById("postpicplace");
-
-//FUNCTION DECLERATIONS
-function displayImages() {
-  postpicplace.textContent = ""; // Clear existing content
-  imageArray.forEach((image, index) => {
-    const imageDiv = document.createElement("div");
-    imageDiv.className = "image";
-
-    const img = document.createElement("img");
-    img.src = URL.createObjectURL(image);
-    img.alt = "image";
-    imageDiv.appendChild(img);
-
-    const span = document.createElement("span");
-    span.textContent = "×";
-    span.onclick = () => deleteImage(index);
-    imageDiv.appendChild(span);
-
-    postpicplace.appendChild(imageDiv);
-  });
-}
-
-
 const quagga = new Quagga();
 
 
 
 
+//EVEN LISTENERS
 // Add an event listener to the submit button for when a file is selected
 subbutton.addEventListener('click', async function() {//Works
 // Get the uploaded file
@@ -70,3 +48,27 @@ const file = fileInput.files[0];
     alert(`Barcode detected: ${result.codeResult.code}`);
   });
 });
+
+
+
+
+//FUNCTION DECLERATIONS
+function displayImages() {
+  postpicplace.textContent = ""; // Clear existing content
+  imageArray.forEach((image, index) => {
+    const imageDiv = document.createElement("div");
+    imageDiv.className = "image";
+
+    const img = document.createElement("img");
+    img.src = URL.createObjectURL(image);
+    img.alt = "image";
+    imageDiv.appendChild(img);
+
+    const span = document.createElement("span");
+    span.textContent = "×";
+    span.onclick = () => deleteImage(index);
+    imageDiv.appendChild(span);
+
+    postpicplace.appendChild(imageDiv);
+  });
+}
