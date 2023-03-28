7893687c1ad5e1215288bbf1e94656fd10e1d1c0
CarlHejlesen <113053807+CarlHejlesen@users.noreply.github.com>
2023-03-28 18:14:10 +0200

    Nothing working Ignore

--- public/barcode/barcode-Carl.js
+++ public/barcode/barcode-Carl.js
@@ -1,63 +1,46 @@
-//CONSTANT DECLERATIONS
-const postpicplace = document.getElementById("postpicplace");
 const fileInput = document.getElementById('file-input');
-const resultDiv = document.getElementById('result');
-const subbutton = document.getElementById('submitbu');
-const showpic = document.getElementById("showpic");
-const quagga = new Quagga();
+const scanButton = document.getElementById('scan-button');
 
+// Initialize Quagga
+Quagga.init({
+  inputStream: {
+    name: 'FileInput',
+    size: 2800, // Image size
+    singleChannel: false,
+  },
+  decoder: {
+    readers: ['ean_reader', 'code_128_reader', 'upc_reader', 'code_39_reader', 'code_39_vin_reader', 'codabar_reader', 'i2of5_reader', '2of5_reader', 'code_93_reader', 'code_ean_8_reader', 'upc_e_reader', 'upc_ean_reader'], // List of barcode types to scan
+  },
+  locate: true,
+  numOfWorkers: 8,
+}, (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-
-
-//EVENT LISTENERS
-subbutton.addEventListener('click', async function () {
-  const file = fileInput.files[0];
-  const image = new Image();
-  image.src = URL.createObjectURL(file);
-  await new Promise((resolve) => {
-    image.onload = resolve;
-  });
-  quagga.init({
-    inputStream: {
-      type: 'ImageStream',
-      src: image,
-    },
-    decoder: {
-      readers: ['ean_reader'],
-    },
-  }, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    quagga.start();
-  });
-  quagga.onDetected((result) => {
-    console.log(result);
-    alert(`Barcode detected: ${result.codeResult.code}`);
-  });
+  // Start Quagga when initialization is complete
+  Quagga.start();
 });
 
+// Listen for file input changes
+fileInput.addEventListener('change', () => {
+  if (fileInput.files && fileInput.files[0]) {
+    // Load the selected file into Quagga
+    Quagga.decodeSingle({
+      src: URL.createObjectURL(fileInput.files[0]),
+      numOfWorkers: 16, // Disable web workers to prevent a CORS error
+    }, (result) => {
+      if (result && result.codeResult) {
+        console.log(`Barcode detected: ${result.codeResult.code}`);
+      } else {
+        console.log('No barcode detected');
+      }
+    });
+  }
+});
 
-
-
-//FUNCTION DECLERATIONS
-function displayImages() {
-  postpicplace.textContent = ""; // Clear existing content
-  imageArray.forEach((image, index) => {
-    const imageDiv = document.createElement("div");
-    imageDiv.className = "image";
-
-    const img = document.createElement("img");
-    img.src = URL.createObjectURL(image);
-    img.alt = "image";
-    imageDiv.appendChild(img);
-
-    const span = document.createElement("span");
-    span.textContent = "×";
-    span.onclick = () => deleteImage(index);
-    imageDiv.appendChild(span);
-
-    postpicplace.appendChild(imageDiv);
-  });
-}
+// Listen for scan button clicks
+scanButton.addEventListener('click', () => {
+  fileInput.click();
+});
