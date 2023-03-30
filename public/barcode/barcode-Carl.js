ef6b97d2718403daa48bfc8c347dbbdb47a287ec
CarlHejlesen <113053807+CarlHejlesen@users.noreply.github.com>
2023-03-30 14:54:03 +0200

    test 2

--- public/barcode/barcode-Carl.js
+++ public/barcode/barcode-Carl.js
@@ -1,63 +1,40 @@
 //CONSTANT DECLERATIONS
-const postpicplace = document.getElementById("postpicplace");
-const fileInput = document.getElementById('file-input');
-const resultDiv = document.getElementById('result');
-const subbutton = document.getElementById('submitbu');
-const showpic = document.getElementById("showpic");
-const quagga = new Quagga();
-
-
-
+const startscanning = document.getElementById("star_scanning");
 
 //EVENT LISTENERS
-subbutton.addEventListener('click', async function () {
-  const file = fileInput.files[0];
-  const image = new Image();
-  image.src = URL.createObjectURL(file);
-  await new Promise((resolve) => {
-    image.onload = resolve;
-  });
-  quagga.init({
-    inputStream: {
-      type: 'ImageStream',
-      src: image,
-    },
-    decoder: {
-      readers: ['ean_13'],
-    },
-  }, (err) => {
-    if (err) {
-      console.error(err);
+startscanning.addEventListener('click', async function () {
+
+console.log("Started Scanning")
+Quagga.init({
+  inputStream : {
+      name : "Live",
+      type : "LiveStream"
+  },
+  decoder : {
+      readers : ["ean_13"]
+  }
+}, function(err) {
+  if (err) {
+      console.log(err);
       return;
-    }
-    quagga.start();
-  });
-  quagga.onDetected((result) => {
-    console.log(result);
-    alert(`Barcode detected: ${result.codeResult.code}`);
-  });
+  }
+  Quagga.start();
+});
+
+Quagga.onDetected(function(result) {
+  var code = result.codeResult.code;
+  console.log("Barcode detected:", code);
+  Quagga.stop();
+  if(confirm("Scan again?")) {
+      Quagga.start();
+  }
+});
 });
 
 
 
 
-//FUNCTION DECLERATIONS
-function displayImages() {
-  postpicplace.textContent = ""; // Clear existing content
-  imageArray.forEach((image, index) => {
-    const imageDiv = document.createElement("div");
-    imageDiv.className = "image";
 
-    const img = document.createElement("img");
-    img.src = URL.createObjectURL(image);
-    img.alt = "image";
-    imageDiv.appendChild(img);
 
-    const span = document.createElement("span");
-    span.textContent = "×";
-    span.onclick = () => deleteImage(index);
-    imageDiv.appendChild(span);
 
-    postpicplace.appendChild(imageDiv);
-  });
-}
+
