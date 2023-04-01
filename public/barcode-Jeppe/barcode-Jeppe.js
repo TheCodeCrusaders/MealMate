5d1aaf7b44dae908d33d0210bf763219f0ae75fe
JeppesGitHub <jreink22@student.aau.dk>
2023-04-01 11:21:38 +0200

    Zxing test

--- public/barcode-Jeppe/barcode-Jeppe.js
+++ public/barcode-Jeppe/barcode-Jeppe.js
@@ -1,63 +1,45 @@
-//CONSTANT DECLERATIONS
-const postpicplace = document.getElementById("postpicplace");
-const fileInput = document.getElementById('file-input');
-const resultDiv = document.getElementById('result');
-const subbutton = document.getElementById('submitbu');
-const showpic = document.getElementById("showpic");
-const quagga = new Quagga();
+document.addEventListener("DOMContentLoaded", function() {
+  const { BrowserMultiFormatReader } = ZXing;
+  const video = document.getElementById("video");
+  const scanButton = document.getElementById("scan-button");
 
+  const reader = new BrowserMultiFormatReader();
+  reader.setPossibleFormats([ZXing.BarcodeFormat.EAN_13]);
+  let scanning = false;
 
+  function startScan() {
+    if (!scanning) {
+      scanning = true;
+      video.style.display = "block";
+      scanButton.textContent = "Stop scanning";
 
-
-//EVENT LISTENERS
-subbutton.addEventListener('click', async function () {
-  const file = fileInput.files[0];
-  const image = new Image();
-  image.src = URL.createObjectURL(file);
-  await new Promise((resolve) => {
-    image.onload = resolve;
-  });
-  quagga.init({
-    inputStream: {
-      type: 'ImageStream',
-      src: image,
-    },
-    decoder: {
-      readers: ['ean_reader'],
-    },
-  }, (err) => {
-    if (err) {
-      console.error(err);
-      return;
+      reader.decodeFromVideoDevice(null, video, (result, err) => {
+        if (result) {
+          console.log("Found barcode:", result.getText());
+          stopScan();
+        }
+        if (err && !(err instanceof ZXing.NotFoundException)) {
+          console.error(err);
+          stopScan();
+        }
+      });
     }
-    quagga.start();
-  });
-  quagga.onDetected((result) => {
-    console.log(result);
-    alert(`Barcode detected: ${result.codeResult.code}`);
+  }
+
+  function stopScan() {
+    if (scanning) {
+      scanning = false;
+      video.style.display = "none";
+      scanButton.textContent = "Start scanning";
+      reader.reset();
+    }
+  }
+
+  scanButton.addEventListener("click", () => {
+    if (scanning) {
+      stopScan();
+    } else {
+      startScan();
+    }
   });
 });
-
-
-
-
-//FUNCTION DECLERATIONS
-function displayImages() {
-  postpicplace.textContent = ""; // Clear existing content
-  imageArray.forEach((image, index) => {
-    const imageDiv = document.createElement("div");
-    imageDiv.className = "image";
-
-    const img = document.createElement("img");
-    img.src = URL.createObjectURL(image);
-    img.alt = "image";
-    imageDiv.appendChild(img);
-
-    const span = document.createElement("span");
-    span.textContent = "×";
-    span.onclick = () => deleteImage(index);
-    imageDiv.appendChild(span);
-
-    postpicplace.appendChild(imageDiv);
-  });
-}
