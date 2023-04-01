2fa33a49b09cff98472392ddc258f8137c4d64c6
JeppesGithub <jreink22@student.aau.dk>
2023-04-01 10:43:16 +0200

    Limbo

--- public/barcode/barcode-Carl.js
+++ public/barcode/barcode-Carl.js
@@ -1,28 +1,56 @@
 //CONSTANT DECLARATIONS
 const startscanning = document.getElementById("start_scanning");
+const viewport = document.querySelector(".viewport");
 
 //EVENT LISTENERS
-startscanning.addEventListener('click', async function () {
-
-  console.log("Started Scanning")
+startscanning.addEventListener("click", async function () {
+  console.log("Started Scanning");
   Quagga.init({
     inputStream: {
       name: "Live",
       type: "LiveStream",
-      target: document.querySelector('#camera')
+      constraints: {
+        width: { min: 640 },
+        height: { min: 480 },
+        facingMode: "environment"
+      },
+      target: viewport,
     },
+    locator: { halfSample: true },
     decoder: {
-      readers: ["ean_reader"]
+      readers: ["ean_reader"],
+      locate: true
     },
-    canvas: {
-      willReadFrequently: true
-    }
+    numOfWorkers: 4,
+    locate: true,
+    patchSize: "medium"
   }, function (err) {
     if (err) {
       console.log(err);
+      alert("Error initializing Quagga: " + err.message);
       return;
     }
     Quagga.start();
+  });
+
+  Quagga.onProcessed(function (result) {
+    if (result) {
+      const drawingCtx = Quagga.canvas.ctx.overlay;
+      const drawingCanvas = Quagga.canvas.dom.overlay;
+      drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+
+      if (result.boxes) {
+        result.boxes.filter(box => box !== result.box).forEach(box => {
+          Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
+        });
+      }
+      if (result.box) {
+        Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
+      }
+      if (result.codeResult && result.codeResult.code) {
+        Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
+      }
+    }
   });
 
   Quagga.onDetected(function (result) {
@@ -38,11 +66,3 @@ startscanning.addEventListener('click', async function () {
     }
   });
 });
-
-
-
-
-
-
-
-
