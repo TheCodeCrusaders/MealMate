587184078b71a9a2bf0b7c008f380951c1d517a1
JeppesGithub <jreink22@student.aau.dk>
2023-04-01 09:32:04 +0200

    Walkthrough

--- public/barcode/barcode-Carl.js
+++ public/barcode/barcode-Carl.js
@@ -1,38 +1,42 @@
-//CONSTANT DECLERATIONS
+//CONSTANT DECLARATIONS
 const startscanning = document.getElementById("start_scanning");
 
 //EVENT LISTENERS
 startscanning.addEventListener('click', async function () {
 
-console.log("Started Scanning")
-Quagga.init({
-  inputStream : {
-      name : "Live",
-      type : "LiveStream",
+  console.log("Started Scanning")
+  Quagga.init({
+    inputStream: {
+      name: "Live",
+      type: "LiveStream",
       target: document.querySelector('#camera')
-  },
-  decoder : {
-      readers : ["ean_reader"]
-  },
-  canvas: {
-    willReadFrequently: true
-}
-}, function(err) {
-  if (err) {
+    },
+    decoder: {
+      readers: ["ean_reader"]
+    },
+    canvas: {
+      willReadFrequently: true
+    }
+  }, function (err) {
+    if (err) {
       console.log(err);
       return;
-  }
-  Quagga.start();
-});
+    }
+    Quagga.start();
+  });
 
-Quagga.onDetected(function(result) {
-  var code = result.codeResult.code;
-  console.log("Barcode detected:", code);
-  Quagga.stop();
-  if(confirm("Scan again?")) {
+  Quagga.onDetected(function (result) {
+    var code = result.codeResult.code;
+    console.log("Barcode detected:", code);
+    Quagga.stop();
+
+    if (code.length > 13) {
+      alert("Error: Barcode contains more than 13 numbers. Please scan again.");
       Quagga.start();
-  }
-});
+    } else if (confirm("Scan again?")) {
+      Quagga.start();
+    }
+  });
 });
 
 
