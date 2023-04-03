f3d3951d049e5498bc6d335f869753e7782bb9a0
mathiaszip <73193562+mathiaszip@users.noreply.github.com>
2023-04-03 18:29:13 +0200

    Shopping list efficiency
    Disabled API call for prices everytime a new item is added, deleted or added to the inventory. Now the user will need to manually click "Click to reload" <- This can only be done once per minute to ensure no spamming.
    
    The prices will load automatically when loading the page.

--- public/shoppinglist/shoppinglist.js
+++ public/shoppinglist/shoppinglist.js
@@ -1,26 +1,72 @@
 const form = document.querySelector("#itemForm");
 const backdrop = document.querySelector("#backdrop");
-
 let refIndex = undefined;
+let canClick = true;
 
 document.addEventListener("DOMContentLoaded", (e) => {
   loadShoppinglist();
-})
+  setTimeout(() => {
+    loadPrices();
+  }, 1000);
+});
+
+function handleClick() {
+    if (canClick) {
+      // Perform button click logic here
+      loadPrices();
+      // Disable button for 1 minute
+      canClick = false;
+      setTimeout(() => {
+        canClick = true;
+      }, 60000);
+    }
+  }
+  
+
+function loadPrices() {
+  const pricePromises = [];
+  const cells = document.querySelectorAll(".cell3");
+  cells.forEach(cell => {
+    const item = cell.parentNode;
+    const pricePromise = fetch(`/api/productPrice?query=${item.querySelector("td:first-child").textContent}`, {})
+      .then(response => response.json())
+      .then(data => {
+        const price = data.suggestions[0].price;
+        cell.textContent = price.toFixed(2) + " kr";
+        cell.style.cursor = "pointer";
+        cell.addEventListener("click", () => {
+          window.open(data.suggestions[0].link, "_blank");
+        });
+        cell.addEventListener("mouseover", () => {
+          cell.setAttribute("title", "Origin: '" + data.suggestions[0].title + "' -> Click to view details");
+        });
+        return price * item.querySelector(".cell2").textContent;
+      })
+      .catch(error => console.error(error));
+    pricePromises.push(pricePromise);
+  });
+  Promise.all(pricePromises)
+    .then(prices => {
+      let total = 0;
+      prices.forEach(price => {
+        total += price;
+      });
+      const totalCell = document.querySelector("#total-cell");
+      totalCell.textContent = total.toFixed(2) + " kr";
+    })
+    .catch(error => console.error(error));
+}
 
 function loadShoppinglist() {
   fetch("/api/shoppingList")
-  .then(response => {
+    .then(response => {
       if (response.ok) {
-          return response.json();
+        return response.json();
       }
       throw new Error("response was not in the 200 range " + response.Error)
-  })
-  .then(data => {
-      // Create table element
+    })
+    .then(data => {
       const table = document.createElement("table");
-
-
-      // Create header row
       const headerRow = document.createElement("tr");
       const header1 = document.createElement("th");
       header1.textContent = "Product";
@@ -35,148 +81,106 @@ function loadShoppinglist() {
       headerRow.appendChild(header3);
 
       const header4 = document.createElement("th");
+      
       header4.textContent = "";
       headerRow.appendChild(header4);
 
-      // Add header row to table
       table.appendChild(headerRow);
 
-      let total = 0;
-      const pricePromises = [];
-
-      // Loop through data and create rows
       data.forEach(item => {
-        let productName = "";
-        let productLink = "";
-          const row = document.createElement("tr");
-          const cell1 = document.createElement("td");
-          cell1.textContent = item.name;
-          row.appendChild(cell1);
+        const row = document.createElement("tr");
+        const cell1 = document.createElement("td");
+        cell1.textContent = item.name;
+        row.appendChild(cell1);
 
-          const cell2 = document.createElement("td");
-          cell2.textContent = item.quantity;
-          row.appendChild(cell2);
+        const cell2 = document.createElement("td");
+        cell2.className = "cell2";
+        cell2.textContent = item.quantity;
+        row.appendChild(cell2);
 
-          const cell3 = document.createElement("td");
-          cell3.className = "cell3";
-          cell3.textContent = "";
-          cell3.style.cursor = "pointer";
-          cell3.addEventListener("click", () => {
-            window.open(productLink, "_blank");
-          });
+        const cell3 = document.createElement("td");
+        cell3.className = "cell3";
 
-          cell3.addEventListener("mouseover", () => {
-            cell3.setAttribute("title", "Origin: '" + productName + "' -> Click to view details");
-          });
+        if (canClick === true) {
+            cell3.style.cursor = "pointer";
+        }
+        else {
+            cell3.style.cursor = "default";
+        }
 
-          row.appendChild(cell3);
+        
+        cell3.textContent = "Click to reload";
 
-          const cell4 = document.createElement("td");
-          const deleteButton = document.createElement("button");
-          deleteButton.textContent = "Delete";
-          deleteButton.onclick = () => {
-            fetch(`/api/shoppingList/${item.id}`, {
+        cell3.addEventListener("click", handleClick);
+
+        row.appendChild(cell3);
+
+        const cell4 = document.createElement("td");
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.onclick = () => {
+          fetch(`/api/shoppingList/${item.id}`, {
               method: 'DELETE'
             })
-              .then(response => {
-                // Remove row from table on successful deletion
-                if (response.ok) {
-                  table.removeChild(row);
-                  setTimeout(() => {
-                    loadShoppinglist();
-                  }, 1000);        
-                }
-              })
-              .catch(error => console.error(error));
-          };
-          cell4.appendChild(deleteButton);
+            .then(response => {
+              if (response.ok) {
+                loadShoppinglist();
+              }
+            })
+            .catch(error => console.error(error));
+        };
+        cell4.appendChild(deleteButton);
 
-          const addToInventory = document.createElement("button");
-          addToInventory.textContent = "Add to inventory";
-          addToInventory.onclick = () => {
-              addNewItem(item.name, item.id);
-          }
+        const addToInventory = document.createElement("button");
+        addToInventory.textContent = "Add to inventory";
+        addToInventory.onclick = () => {
+          addNewItem(item.name, item.id);
+        };
+        cell4.appendChild(addToInventory);
 
-          cell4.appendChild(addToInventory);
-
-          row.appendChild(cell4);
-
-          const pricePromise = fetch(`/api/productPrice?query=${item.name}`, {
-          })
-          .then(response => response.json())
-          .then(data => {
-            // Get the price for the first product in the suggestions array
-            const price = data.suggestions[0].price;
-
-            cell3.textContent = price.toFixed(2) + " kr";
-            if (productName === "") {
-              productName = data.suggestions[0].title;
-            }
-
-            if (productLink === "") {
-              productLink = data.suggestions[0].link;
-            }
-
-            return price * item.quantity;
-
-          })
-          .catch(error => console.error(error));
-        // Add row to table
-
-        pricePromises.push(pricePromise);
-
+        row.appendChild(cell4);
         table.appendChild(row);
       });
-      Promise.all(pricePromises)
-      .then(prices => {
-        prices.forEach(price => {
-          total += price;
-        });
-        // Add total row to table
-        const totalRow = document.createElement("tr");
-        const totalCell1 = document.createElement("td");
-        totalCell1.textContent = "Total";
-        totalCell1.style.fontWeight = "bold";
-        totalRow.appendChild(totalCell1);
 
-        const totalCell2 = document.createElement("td");
-        totalCell2.textContent = "";
-        totalRow.appendChild(totalCell2);
+      const totalRow = document.createElement("tr");
+      const totalCell1 = document.createElement("td");
+      totalCell1.textContent = "Total";
+      totalCell1.style.fontWeight = "bold";
+      totalRow.appendChild(totalCell1);
 
-        const totalCell3 = document.createElement("td");
-        totalCell3.textContent = total.toFixed(2) + " kr";
-        totalCell3.style.fontWeight = "bold";
-        totalRow.appendChild(totalCell3);
+      const totalCell2 = document.createElement("td");
+      totalRow.appendChild(totalCell2);
 
-        const totalCell4 = document.createElement("td");
-        totalRow.appendChild(totalCell4);
+      const totalCell3 = document.createElement("td");
+      totalCell3.id = "total-cell";
+      totalCell3.textContent = "0 kr";
+      totalCell3.style.fontWeight = "bold";
+      totalRow.appendChild(totalCell3);
 
-        // Add total row to table
-        table.appendChild(totalRow);
-      })
-      .catch(error => console.error(error));
+      const totalCell4 = document.createElement("td");
+      totalRow.appendChild(totalCell4);
 
-      // Add table to HTML page
+      table.appendChild(totalRow);
+
       const tableContainer = document.getElementById("shoppinglist");
       tableContainer.innerHTML = "";
       tableContainer.appendChild(table);
-  })
-  .catch(error => console.error(error));
+    })
+    .catch(error => console.error(error));
 }
 
 const addItemForm = document.getElementById('shoppinglist-form');
 addItemForm.addEventListener('submit', function(event) {
   event.preventDefault();
-  const formData = new FormData(addItemForm); // Get the form data
-
+  const formData = new FormData(addItemForm);
   const quantityInput = formData.get('shoppinglist-amount');
   const parsedQuantity = parseInt(quantityInput);
   let itemData = {};
 
   if (!isNaN(parsedQuantity) && parsedQuantity >= 0 && parsedQuantity <= 1000) {
     itemData = {
-      name: formData.get('shoppinglist-input'), // Replace 'username' with the name of your username input field
-      quantity: parsedQuantity // Replace 'password' with the name of your password input field
+      name: formData.get('shoppinglist-input'),
+      quantity: parsedQuantity
     };
   } else {
     alert("Please enter a number between 1 and 1000 for the quantity.");
@@ -184,52 +188,42 @@ addItemForm.addEventListener('submit', function(event) {
   }
 
   fetch('/api/shoppingList', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(itemData)
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    // Update the shopping list in the DOM
-    setTimeout(() => {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(itemData)
+    })
+    .then(response => response.json())
+    .then(data => {
         loadShoppinglist();
-      }, 1000);
-
-  })
-  .catch(error => {
-    console.error(error);
-
-    setTimeout(() => {
-        loadShoppinglist();
-      }, 1000);
-  });
+    })
+    .catch(error => {
+      console.error(error);
+      loadShoppinglist();
+    });
 });
 
 function addNewItemToPersonalList(itemId) {
   if (refIndex === undefined) {
-
     let data = {
       "location": form.location.value,
       "name": form.name.value,
       "expirationDate": form.expirationDate.value
     };
     fetch("/API/postlist", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    })
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
       .then(response => {
         if (response.ok) {
           fetch(`/api/shoppingList/` + itemId, {
-            method: 'DELETE'
-          })
+              method: 'DELETE'
+            })
             .then(response => {
-              // Remove row from table on successful deletion
               if (response.ok) {
                 loadShoppinglist();
               }
@@ -245,15 +239,12 @@ function addNewItemToPersonalList(itemId) {
   }
 }
 
-
-//addNewItem() function decleration
 function addNewItem(itemName, itemId) {
   form.classList.toggle("visible");
   if (backdrop.style.display === "block") {
-      backdrop.style.display = "none";
-  }
-  else {
-      backdrop.style.display = "block";
+    backdrop.style.display = "none";
+  } else {
+    backdrop.style.display = "block";
   }
 
   document.getElementById("name").value = itemName;
@@ -261,9 +252,11 @@ function addNewItem(itemName, itemId) {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     addNewItemToPersonalList(itemId);
-   });
+    addNewItem();
+  });
 }
+
 backdrop.addEventListener("click", (e) => {
   addNewItem();
   refIndex = undefined;
-})
+});
