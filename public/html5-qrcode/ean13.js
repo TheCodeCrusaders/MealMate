4c8087926aab6e61e719357b971a73245bfc417d
JeppesGithub <jreink22@student.aau.dk>
2023-04-05 17:08:32 +0200

    Fuckaround

--- public/html5-qrcode/ean13.js
+++ public/html5-qrcode/ean13.js
@@ -1,3 +1,14 @@
+function onScanSuccess(decodedText, decodedResult) {
+  // handle the scanned code as you like, for example:
+  console.log(`Code matched = ${decodedText}`, decodedResult);
+}
+
+function onScanFailure(error) {
+  // handle scan failure, usually better to ignore and keep scanning.
+  // for example:
+  console.warn(`Code scan error = ${error}`);
+}
+
 async function requestCameraPermission() {
   try {
     await navigator.mediaDevices.getUserMedia({ video: true });
