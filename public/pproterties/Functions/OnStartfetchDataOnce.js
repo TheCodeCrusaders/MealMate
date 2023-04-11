cbbecf53a22723f9c9df30b06c43ae7eb0dd53ac
JeppesGithub <jreink22@student.aau.dk>
2023-04-11 17:42:27 +0200

    Bøvl med at putte kode i kasser

---
+++ public/pproterties/Functions/OnStartfetchDataOnce.js
@@ -0,0 +1,20 @@
+//Var.
+let private_user_Item_property_data;
+
+//Func.
+export function OnStartfetchDataOnce() {
+    fetch("/API/GetPrivateProtertyList")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("response was not in the 200 range ")
+      })
+      .then((data) => {
+        private_user_Item_property_data = data
+        createTable(data)
+      })
+      .catch((error) => {
+        alert("An error occured");
+      });
+  }
