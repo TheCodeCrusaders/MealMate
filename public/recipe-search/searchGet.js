6830399d495919ef48da59b420437cc23a84a41c
MShadiF <mfares22@student.aau.dk>
2023-05-17 15:18:57 +0200

    Bug fix, redirect and empty search array

--- public/recipe-search/searchGet.js
+++ public/recipe-search/searchGet.js
@@ -28,7 +28,14 @@ function fetchForItemsSaved() {
         referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         body: JSON.stringify(data), // body data type must match "Content-Type" header
     })
-        .then(result => result.json())
+        .then(response => {
+            if (response.redirected) {
+                window.location.href = response.url;
+            }
+            if (response.ok) {
+                return response.json()
+            }
+        })
         .then(jsonData => listCreation(jsonData))
         .catch(error => console.log(error))
 }
@@ -38,27 +45,27 @@ function listCreation(recipes) {
     const allList = document.getElementById('resultList');
     allList.textContent = '';
     recipes.forEach(ULelement => {
-      const recipieListOrder = document.createElement('ul');
-      recipieListOrder.textContent = `${ULelement.nameOfRecipe} ${ULelement.score} / ${ULelement.ingredients.length}`;
-      ULelement.ingredients.forEach(LIelement => {
-        const listIngredients = document.createElement('li');
-        listIngredients.textContent = `${LIelement.ingredient} amount: ${LIelement.amount}`
-        recipieListOrder.appendChild(listIngredients);
-      });
-      ULelement.instructions.forEach(instelement => {
-        const listInstructions = document.createElement('li');
-        listInstructions.textContent = instelement.inst;
-        recipieListOrder.appendChild(listInstructions)
-      });
-      allList.appendChild(recipieListOrder);
+        const recipieListOrder = document.createElement('ul');
+        recipieListOrder.textContent = `${ULelement.nameOfRecipe} ${ULelement.score} / ${ULelement.ingredients.length}`;
+        ULelement.ingredients.forEach(LIelement => {
+            const listIngredients = document.createElement('li');
+            listIngredients.textContent = `${LIelement.ingredient} amount: ${LIelement.amount}`
+            recipieListOrder.appendChild(listIngredients);
+        });
+        ULelement.instructions.forEach(instelement => {
+            const listInstructions = document.createElement('li');
+            listInstructions.textContent = instelement.inst;
+            recipieListOrder.appendChild(listInstructions)
+        });
+        allList.appendChild(recipieListOrder);
     });
 }
-  
+
 
 function createButton() {
     const itemName = document.createElement('span');
     let searchMethod = document.querySelector('#search').value;
-    if (!itemsSaved.includes(searchMethod) && isNaN(Number(searchMethod))) {
+    if (!itemsSaved.includes(searchMethod) && isNaN(Number(searchMethod)) && searchMethod.length !== 0) {
         itemName.textContent = `[${searchMethod}]`;
         itemName.className = 'item-name';
         itemsSaved.push(document.querySelector('#search').value);
